e3fcbbd688dfcceceb0cddae4bb68d3449b33cb8
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-01-27 18:21:05 +0100

    buttons

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,20 +8,21 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Wigell's Bakery");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
