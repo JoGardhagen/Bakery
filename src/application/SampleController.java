a4e9dbd81f7556657405a89ad5656c5f75a00491
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-02-03 18:58:15 +0100

    tillsnyggning

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -15,58 +15,54 @@ public class SampleController {
 	@FXML
 	private Button orderBtn;
 	@FXML
-	private RadioButton PrincessCake,ChoclateCake,ShrovetideBun;
+	private RadioButton PrincessCake, ChoclateCake, ShrovetideBun;
 	@FXML
 	private TextField customerField;
 	@FXML
-	private Label orderLable,orderIsDoneLabel;
-	
+	private Label orderLable, orderIsDoneLabel;
+
 	WigelsBakery wb = new WigelsBakery();// frabrik Instance för bageriet
 	String cakeDone = "Cake is now done,";
+
 	public void orderNow(ActionEvent event) throws InterruptedException {
 		orderBtnClicked();
 	}
+
 	public void orderBtnClicked() throws InterruptedException {
 		String custName = customerField.getText();
 		Customer cust = new Customer(custName);
 		wb.addCustomerToList(cust);
-		if(PrincessCake.isSelected()) {
-		
+		if (PrincessCake.isSelected()) {
+
 			Cakes cake = wb.orderCake("Princess Cake");
-//			wb.attach(wb.);
-//			System.out.println(cake.getName()+" "+cust.getName()+"\n\n");
-			orderLable.setText(cust.getName() + " Ordered "+ cake.getName());
-//			orderLable.setText(BakingPipeline.super().cake.);
-			if(cake.isBaked()) {
+			orderLable.setText(cust.getName() + " Ordered " + cake.getName());
+			if (cake.isBaked()) {
 				wb.notifyUpdate(cakeDone);
-				orderIsDoneLabel.setText(wb.VD.message+" "+cake.getName()+", "+cust.getName());
+				orderIsDoneLabel.setText(wb.VD.message + " " + cake.getName() + ", " + cust.getName());
 			}
-			
+
 		}
-		if(ChoclateCake.isSelected()) {
-		
+		if (ChoclateCake.isSelected()) {
+
 			Cakes cake = wb.orderCake("Choclate Cake");
-//			System.out.println(cake.getName()+" "+cust.getName()+"\n\n");
-			orderLable.setText(cust.getName() + " Ordered "+ cake.getName());
-			if(cake.isBaked()) {
-				wb.notifyUpdate(cakeDone);	
-				orderIsDoneLabel.setText(wb.VD.message+" "+cake.getName()+", "+cust.getName());
-			}
-		}
-		if(ShrovetideBun.isSelected()) {
-			
-			Cakes cake = wb.orderCake("Shrovetide Bun");
-//			System.out.println(cake.getName()+" "+cust.getName()+"\n\n");
-			orderLable.setText(cust.getName() + " Ordered "+ cake.getName());
-			if(cake.isBaked()) {
+			orderLable.setText(cust.getName() + " Ordered " + cake.getName());
+			if (cake.isBaked()) {
 				wb.notifyUpdate(cakeDone);
-				orderIsDoneLabel.setText(wb.VD.message+" "+cake.getName()+", "+cust.getName());
+				orderIsDoneLabel.setText(wb.VD.message + " " + cake.getName() + ", " + cust.getName());
 			}
 		}
-	
+		if (ShrovetideBun.isSelected()) {
+
+			Cakes cake = wb.orderCake("Shrovetide Bun");
+			orderLable.setText(cust.getName() + " Ordered " + cake.getName());
+			if (cake.isBaked()) {
+				wb.notifyUpdate(cakeDone);
+				orderIsDoneLabel.setText(wb.VD.message + " " + cake.getName() + ", " + cust.getName());
+			}
+		}
+
 		wb.printCustomerList();
-		
+
 	}
-	
-	
+
 }
